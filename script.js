62f17cd0999d6d246bd74a48b24c52e30ab73696
langarus <langarus@gmail.com>
2020-06-22 11:00:35 +0300

    The gallery is generated with JS, the slider buttons redone

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ function callBack(entries){
 let observer = new IntersectionObserver(callBack, options)
 
 observer.observe(galleries)
+
+for(let i = 1; i <= 9; i++){
+    const mainContainer = document.querySelector(".grid-container")
+const blogDiv = document.createElement("div")
+blogDiv.classList = "blog-posts"
+blogDiv.innerHTML = `<a href="#">
+<div class="overlay">
+    <div class="text">
+        <p class="date">31/01/2020</p>
+        <p class = "name">Family Name</p>
+        <p class="open">Open Post</p>
+    </div>
+    <div class="full-background"></div>
+</div>   
+    <img class = "gallery1" src="styles/thumbnails/${i}.jpg" alt="">                    
+</a>`
+mainContainer.appendChild(blogDiv)
+}
